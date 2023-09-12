c0110245b2405fe565d93711ce9d46593937a533
Felipe Barcelos <felipebarcelospro@gmail.com>
2023-09-12 16:43:20 -0300

    feat: initial deploy

--- apps/web/src/lib/get-asset-url.ts
+++ apps/web/src/lib/get-asset-url.ts
@@ -1,6 +1,3 @@
 export function getAssetUrl(assetId: string): string {
-  const strapiAPIURL =
-    process.env.NEXT_PUBLIC_STRAPI_API_URL || 'http://127.0.0.1:1337'
-
-  return `${strapiAPIURL}${assetId}`
+  return assetId
 }
