fb8a4174c32165ae03133d4c34fe0428f664c9ae
Felipe Barcelos <felipebarcelospro@gmail.com>
2023-09-12 23:44:44 -0300

    feat: initial deploy

--- apps/web/src/components/logo/index.tsx
+++ apps/web/src/components/logo/index.tsx
@@ -2,11 +2,16 @@
 export function Logo() {
   return (
     <>
-      <img className="h-16 hidden lg:block" src="/logo.svg" alt="" />
+      <img className="h-16 hidden lg:block" src="/logo.svg" alt=""
+        width="132"
+        height="64"
+      />
       <img
         className="h-10 lg:hidden"
         src="/logo-only-icon.png"
         alt=""
+        width="132"
+        height="64"
       />
     </>
   )
