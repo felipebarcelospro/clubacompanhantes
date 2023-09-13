1c84e5b7a4f32f9f943ee7964372d38049f59ce0
Felipe Barcelos <felipebarcelospro@gmail.com>
2023-09-13 01:54:19 -0300

    feat: initial deploy

--- apps/web/src/app/sitemap.tsx
+++ apps/web/src/app/sitemap.tsx
@@ -1,6 +1,13 @@
+import { fetchStrapi } from "@/lib/strapi"
+import { Post } from "@/types/post"
+
 export default async function sitemap() {
-  const routes = ['', '/about', '/blog'].map((route) => ({
-    url: `https://acme.com${route}`,
+  const posts = await fetchStrapi({
+    path: '/posts',
+  }) as Post[]
+
+  const routes = ['/', posts.map(post => `/acompanhantes/${post.slug}`)].map((route) => ({
+    url: `https://clubacompanhantes.com${route}`,
     lastModified: new Date().toISOString(),
   }))
 
